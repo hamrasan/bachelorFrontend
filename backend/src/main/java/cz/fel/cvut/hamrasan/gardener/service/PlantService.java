6d154ea50a05d77a369680f5d92b2cf073c4120b
Sandra Hamráková <hamrasan@fel.cvut.cz>
2021-02-20 15:41:42 +0100

    userService

---
+++ backend/src/main/java/cz/fel/cvut/hamrasan/gardener/service/PlantService.java
@@ -0,0 +1,8 @@
+package cz.fel.cvut.hamrasan.gardener.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlantService {
+
+}
--- backend/src/main/java/cz/fel/cvut/hamrasan/gardener/service/UserService.java
+++ backend/src/main/java/cz/fel/cvut/hamrasan/gardener/service/UserService.java
@@ -1,4 +1,51 @@
 package cz.fel.cvut.hamrasan.gardener.service;
 
+import cz.fel.cvut.hamrasan.gardener.dao.UserDao;
+import cz.fel.cvut.hamrasan.gardener.exceptions.BadPassword;
+import cz.fel.cvut.hamrasan.gardener.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
 public class UserService {
+
+    UserDao userDao;
+
+    @Autowired
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+
+    public User find(Long id) {
+        return userDao.find(id);
+    }
+
+
+    public User find(String name) {
+        return userDao.findByEmail(name);
+    }
+
+    @Transactional
+    public void createUser(User user, String passwordAgain) throws BadPassword {
+        Objects.requireNonNull(user);
+        if (!user.getPassword().equals(passwordAgain)) throw new BadPassword();
+        user.encodePassword();
+
+        userDao.persist(user);
+    }
+
+
+
+
 }
