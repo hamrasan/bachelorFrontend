6b8a84c17269c4534c82c66311881c6517e71aa0
Sandra Hamráková <hamrasan@fel.cvut.cz>
2021-04-16 01:11:08 +0200

    Valve findByName

--- backend/src/main/java/cz/fel/cvut/hamrasan/gardener/dao/ValveDao.java
+++ backend/src/main/java/cz/fel/cvut/hamrasan/gardener/dao/ValveDao.java
@@ -3,8 +3,19 @@ package cz.fel.cvut.hamrasan.gardener.dao;
 import cz.fel.cvut.hamrasan.gardener.model.Valve;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NoResultException;
+
 @Repository
 public class ValveDao extends BaseDao<Valve> {
 
     public ValveDao(){super(Valve.class);}
+
+    public Valve findByName(String name) {
+        try {
+            return em.createNamedQuery("Valve.findByName", Valve.class).setParameter("name", name)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 }
